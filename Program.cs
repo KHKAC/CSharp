6c03b211ed196afd642c2df5ffa0b9db1d35d678
KHKAmuta <hagoonfinale@gmail.com>
2025-07-04 15:03:14 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 {
     private static void Main(string[] args)
     {
+<<<<<<< HEAD
         Console.WriteLine("Hello, anyone!");
+=======
+        Console.WriteLine("Hello, aaaaaHoonkyu!");
+>>>>>>> parent of c6a64ed (Update Program.cs)
     }
 }
